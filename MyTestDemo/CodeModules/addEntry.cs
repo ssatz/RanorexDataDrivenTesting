da9849c309c0e01b3500aa997ec8440f68a4dd7d
satz <sathish.thi@gmail.com>
2019-06-26 12:08:43 +0530

    Bump:) Initial commit

---
+++ MyTestDemo/CodeModules/addEntry.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by Ranorex
+ * User: ADMIN
+ * Date: 26-06-2019
+ * Time: 11:21 AM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace MyTestDemo.CodeModules
+{
+    /// <summary>
+    /// Description of addEntry.
+    /// </summary>
+    [TestModule("C7262A48-AB4E-4911-868D-5826B6B4D2E4", ModuleType.UserCode, 1)]
+    public class addEntry : ITestModule
+    {
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public addEntry()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+            MyTestDemoRepository myTest = new MyTestDemoRepository();
+            var button = myTest.RxMainFrame.BtnAddPerson;
+            button.Click();
+        }
+    }
+}
---
+++ MyTestDemo/CodeModules/validateEntry.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by Ranorex
+ * User: ADMIN
+ * Date: 26-06-2019
+ * Time: 11:27 AM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace MyTestDemo.CodeModules
+{
+    /// <summary>
+    /// Description of validateEntry.
+    /// </summary>
+    [TestModule("AF81A1CD-E2CA-453E-A529-77FD8604643F", ModuleType.UserCode, 1)]
+    public class validateEntry : ITestModule
+    {
+    string _itemEntry = "1";
+    [TestVariable("6077a88b-20f8-497e-8257-c82e024f8d1c")]
+    public string itemEntry
+    {
+    	get { return _itemEntry; }
+    	set { _itemEntry = value; }
+    }
+    
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        public validateEntry()
+        {
+            // Do not delete - a parameterless constructor is required!
+        }
+
+        /// <summary>
+        /// Performs the playback of actions in this module.
+        /// </summary>
+        /// <remarks>You should not call this method directly, instead pass the module
+        /// instance to the <see cref="TestModuleRunner.Run(ITestModule)"/> method
+        /// that will in turn invoke this method.</remarks>
+        void ITestModule.Run()
+        {
+            Mouse.DefaultMoveTime = 300;
+            Keyboard.DefaultKeyPressTime = 100;
+            Delay.SpeedFactor = 1.0;
+            
+              MyTestDemoRepository myTest = new MyTestDemoRepository();
+              
+              if(Validate.Equals(myTest.RxMainFrame.LblNumberOfPersonsNumber.TextValue,itemEntry)){
+                 	Report.Success("Validation","Correct");
+                 }else{
+                 	Report.Success("Validation","Entry number wrong!");
+                 	               
+                 }
+        }
+    }
+}
